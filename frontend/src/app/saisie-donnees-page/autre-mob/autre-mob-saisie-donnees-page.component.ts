a5032cbe83749dcc9f9492a30d55723568969094
Clovis CHATRY <clovis.chatry@student.junia.com>
2025-04-28 16:31:20 +0200

    Hugo header saisie donnees (#4)
    * Add login (no logout yet)
    
    * barre de navigation et header saisie donnees
    
    * feat: creation tableau tab energie
    
    * feat: renommer bouton dashboard
    
    * feat: ajout endpoint file
    
    * feat: renommage saisie donnees energie
    
    * feat: resolution bug unite valeur
    
    * feat: redirection header saisie donnees
    
    * feat:changement url endpoint
    
    * feat: ajout fond bleu clair
    
    * feat: creation directory onglets
    
    * feat: refactor name
    
    ---------
    
    Co-authored-by: Hugo  Sanson <hugo.sanson@student.junia.com>

---
+++ frontend/src/app/saisie-donnees-page/autre-mob/autre-mob-saisie-donnees-page.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-saisie-donnees-page',
+  standalone: true,
+  templateUrl: './autre-mob-saisie-donnees-page.component.html',
+  styleUrls: ['./autre-mob-saisie-donnees-page.component.scss'],
+})
+export class AutreMobSaisieDonneesPageComponent {
+
+}
