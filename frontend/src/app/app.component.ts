63dd20b10b9960a2dc8a25d6dbc423fa4f91ab3a
Hugo Sanson <hugo.sanson@student.junia.com>
2025-04-30 10:21:55 +0200

    emmsisions fugitives (#7)
    * Add login (no logout yet)
    
    * barre de navigation et header saisie donnees
    
    * feat: creation tableau tab energie
    
    * feat: renommer bouton dashboard
    
    * feat: ajout endpoint file
    
    * feat: renommage saisie donnees energie
    
    * feat: redirection header saisie donnees
    
    * feat: ajout fond bleu clair
    
    * feat: creation directory onglets
    
    * feat: refactor name
    
    * feat: page emissions fugitives
    
    * feat: finalisation page emiss fugi
    
    * feat: corrections johanne
    
    * feat: retour mr
    
    ---------
    
    Co-authored-by: clovis.chatry <clovis.chatry@student.junia.com>

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,19 +1,36 @@
 import {Component} from '@angular/core';
 import {HeaderComponent} from './components/header/header.component';
-import {Router, RouterModule} from '@angular/router';
+
+import {RouterOutlet} from '@angular/router';
+import {ActivatedRoute, NavigationEnd, Router, RouterModule} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {HeaderSaisieDonneesComponent} from './header-saisie-donnees/header-saisie-donnees.component';
+import {filter, map, mergeMap} from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, HeaderComponent, RouterModule, HeaderSaisieDonneesComponent],
+
+  imports: [CommonModule, RouterOutlet, HeaderComponent, RouterModule, HeaderSaisieDonneesComponent],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  showSaisieHeader = false;
   title = 'frontend';
-  constructor(private router: Router) {}
-  isSaisiePage(): boolean {
-    return this.router.url.includes('energieOnglet'); // Remplace par ton path de saisie des données
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(() => this.activatedRoute),
+        map(route => {
+          while (route.firstChild) route = route.firstChild;
+          return route;
+        }),
+        mergeMap(route => route.data)
+      )
+      .subscribe(data => {
+        this.showSaisieHeader = !!data['showSaisieHeader'];
+      });
   }
 }
