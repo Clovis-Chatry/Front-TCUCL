b6996c6b0da314561fce25cb7861cada5b7351e6
clovis.chatry <clovis.chatry@student.junia.com>
2025-04-30 14:49:06 +0200

    reactivating authguard

--- frontend/src/app/guards/authguard.ts
+++ frontend/src/app/guards/authguard.ts
@@ -3,14 +3,13 @@ import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  //const authService = inject(AuthService);
-  //const router = inject(Router);
-  return true;
+  const authService = inject(AuthService);
+  const router = inject(Router);
 
-  //if (authService.isAuthenticated()) {
-  //  return true;
-  //} else {
-  //  router.navigate(['/login']);
-  //  return false;
-  //}
+  if (authService.isAuthenticated()) {
+    return true;
+  } else {
+    router.navigate(['/login']);
+    return false;
+  }
 };
