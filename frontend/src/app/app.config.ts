a9d6a22d1a5e7ade1eb8f62a504df2a2461b6eaa
Hugo Sanson <hugo.sanson@student.junia.com>
2025-04-30 10:21:55 +0200

    emmsisions fugitives (#7)
    * Add login (no logout yet)
    
    * barre de navigation et header saisie donnees
    
    * feat: creation tableau tab energie
    
    * feat: renommer bouton dashboard
    
    * feat: ajout endpoint file
    
    * feat: renommage saisie donnees energie
    
    * feat: redirection header saisie donnees
    
    * feat: ajout fond bleu clair
    
    * feat: creation directory onglets
    
    * feat: refactor name
    
    * feat: page emissions fugitives
    
    * feat: finalisation page emiss fugi
    
    * feat: corrections johanne
    
    * feat: retour mr
    
    ---------
    
    Co-authored-by: clovis.chatry <clovis.chatry@student.junia.com>

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 // import { routes } from './app.routes';
+
 import {EnergieSaisieDonneesPageComponent} from './saisie-donnees-page/energie/energie-saisie-donnees-page.component';
 import {LoginPageComponent} from './components/login-page/login-page.component';
 import {HeaderSaisieDonneesComponent} from './header-saisie-donnees/header-saisie-donnees.component';
@@ -10,7 +11,9 @@ export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter([
     { path: 'login', component: LoginPageComponent },
     { path: 'header-saisie', component: HeaderSaisieDonneesComponent },
+
     { path: 'saisie-donnees/:id', component: EnergieSaisieDonneesPageComponent },
+
     { path: '', redirectTo: '/login', pathMatch: 'full' }
   ])]
 };
