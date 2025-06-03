af2d2d717e3adda13e7120675d16a55746ee401a
Clovis CHATRY <clovis.chatry@student.junia.com>
2025-06-03 16:27:46 +0200

    page saisie dechets & fixes (#18)

--- frontend/src/app/models/typeMachine.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum TypeMachineEnum {
-  ARMOIRE = "Armoire",
-  DRV = "DRV",
-  "EAU_GLACEE_MOINS_50KW" = "EAU_GLACEE_MOINS_50KW",
-  "EAU_GLACEE_PLUS_50KW" = "EAU_GLACEE_PLUS_50KW",
-  INCONNU = "Inconnu",
-  NA = "NA"
-}
