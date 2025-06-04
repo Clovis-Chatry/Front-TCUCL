8f55875b7550e12b2441463c6d4040712a67160e
Hugo  Sanson <hugo.sanson@student.junia.com>
2025-06-04 16:35:44 +0200

    feat: correction emiss fugitives

--- frontend/src/app/components/saisie-donnees-page/emiss-fugi/emiss-fugi-saisie-donnees-page.component.ts
+++ frontend/src/app/components/saisie-donnees-page/emiss-fugi/emiss-fugi-saisie-donnees-page.component.ts
@@ -25,7 +25,79 @@ export class EmissFugiSaisieDonneesPageComponent implements OnInit {
   emmissionFugitiveOngletId: string = '';
   noData: boolean = false;
   hasError: boolean = false;
+  fluideTypes = Object.values(TypeFluide);
 
+  fluideTypesLibelles = [
+    { value: TypeFluide.CH4, label: "CH4" },
+    { value: TypeFluide.N20, label: "N20" },
+    { value: TypeFluide.R134a, label: "R134a" },
+    { value: TypeFluide.R404a, label: "R404a" },
+    { value: TypeFluide.R407a, label: "R407a" },
+    { value: TypeFluide.R407c, label: "R407c" },
+    { value: TypeFluide.R410a, label: "R410a" },
+    { value: TypeFluide.R417a, label: "R417a" },
+    { value: TypeFluide.R422a, label: "R422a" },
+    { value: TypeFluide.R422d, label: "R422d" },
+    { value: TypeFluide.R427a, label: "R427a" },
+    { value: TypeFluide.R507, label: "R507" },
+    { value: TypeFluide.R507a, label: "R507a" },
+    { value: TypeFluide.HFC_125, label: "HFC - 125" },
+    { value: TypeFluide.HFC_134, label: "HFC - 134" },
+    { value: TypeFluide.HFC_134a, label: "HFC - 134a" },
+    { value: TypeFluide.HFC_143, label: "HFC - 143" },
+    { value: TypeFluide.HFC_143a, label: "HFC - 143a" },
+    { value: TypeFluide.HFC_152a, label: "HFC - 152a" },
+    { value: TypeFluide.HFC_227ea, label: "HFC - 227ea" },
+    { value: TypeFluide.HFC_23, label: "HFC - 23" },
+    { value: TypeFluide.HFC_236fa, label: "HFC - 236fa" },
+    { value: TypeFluide.HFC_245ca, label: "HFC - 245ca" },
+    { value: TypeFluide.HFC_32, label: "HFC - 32" },
+    { value: TypeFluide.HFC_41, label: "HFC - 41" },
+    { value: TypeFluide.HFC_43_10mee, label: "HFC - 43 - 10mee" },
+    { value: TypeFluide.PERFLUOROBUTANE_R3110, label: "Perfluorobutane = R3110" },
+    { value: TypeFluide.PERFLUOROMETHANE_R14, label: "Perfluoromethane = R14" },
+    { value: TypeFluide.PERFLUOROPROPANE_R218, label: "Perfluoropropane = R218" },
+    { value: TypeFluide.PERFLUOROPENTANE_R4112, label: "Perfluoropentane = R4112" },
+    { value: TypeFluide.PERFLUOROCYCLOBUTANE_R318, label: "Perfluorocyclobutane = R318" },
+    { value: TypeFluide.PERFLUOROETHANE_R116, label: "Perfluoroethane = R116" },
+    { value: TypeFluide.PERFLUOROHEXANE_R5114, label: "Perfluorohexane = R5114" },
+    { value: TypeFluide.SF6, label: "SF6" },
+    { value: TypeFluide.NF3, label: "NF3" },
+    { value: TypeFluide.R11_CFC_HORS_KYOTO, label: "R11 - CFC hors kyoto" },
+    { value: TypeFluide.R12_CFC_HORS_KYOTO, label: "R12 - CFC hors kyoto" },
+    { value: TypeFluide.R113, label: "R113" },
+    { value: TypeFluide.R114, label: "R114" },
+    { value: TypeFluide.R115, label: "R115" },
+    { value: TypeFluide.R122, label: "R122" },
+    { value: TypeFluide.R122a, label: "R122a" },
+    { value: TypeFluide.R123, label: "R123" },
+    { value: TypeFluide.R123a, label: "R123a" },
+    { value: TypeFluide.R124, label: "R124" },
+    { value: TypeFluide.R13, label: "R13" },
+    { value: TypeFluide.R132c, label: "R132c" },
+    { value: TypeFluide.R141b, label: "R141b" },
+    { value: TypeFluide.R142b, label: "R142b" },
+    { value: TypeFluide.R21, label: "R21" },
+    { value: TypeFluide.R225ca, label: "R225ca" },
+    { value: TypeFluide.R225cb, label: "R225cb" },
+    { value: TypeFluide.R502_CFC_HORS_KYOTO, label: "R502 - CFC hors kyoto" },
+    { value: TypeFluide.R22_HCFC_HORS_KYOTO, label: "R22 - HCFC hors kyoto" },
+    { value: TypeFluide.R401a_HCFC_HORS_KYOTO, label: "R401a - HCFC hors kyoto" },
+    { value: TypeFluide.R408a_HCFC_HORS_KYOTO, label: "R408a - HCFC hors kyoto" },
+    { value: TypeFluide.TETRACHLOROMETHANE, label: "Tétrachlorométhane" },
+    { value: TypeFluide.NOx, label: "NOx" },
+    { value: TypeFluide.BROMURE_DE_METHYLE, label: "Bromure de méthyle" },
+    { value: TypeFluide.CHLOROFORME_DE_METHYLE, label: "Chloroforme de méthyle" },
+    { value: TypeFluide.HALON_1211, label: "Halon 1211" },
+    { value: TypeFluide.HALON_1301, label: "Halon 1301" },
+    { value: TypeFluide.HALON_2402, label: "Halon 2402" },
+    { value: TypeFluide.DICHLOROMETHANE, label: "Dichlorométhane" },
+  ]
+
+    getLibelleTypeFluide(type: string): string {
+    const item = this.fluideTypesLibelles.find(t => t.value === type);
+    return item ? item.label : type;
+  }
   // TypeFluideLabelEntries contenant les objets { value, label }
   typeFluideLabelEntries = Object.keys(TypeFluide).map(key => {
     return { value: TypeFluide[key as keyof typeof TypeFluide], label: TypeFluideLabels[TypeFluide[key as keyof typeof TypeFluide]] };
@@ -50,8 +122,10 @@ export class EmissFugiSaisieDonneesPageComponent implements OnInit {
   constructor(private emmissionsFugtivesService: EmmissionsFugtivesService) {}
 
   ngOnInit() {
-    this.emmissionFugitiveOngletId = this.route.snapshot.paramMap.get('id')!;
-    this.loadMachines();
+    this.route.paramMap.subscribe(params => {
+      this.emmissionFugitiveOngletId = params.get('id') || '';
+      this.loadMachines();
+    });
   }
 
   loadMachines() {
@@ -65,7 +139,7 @@ export class EmissFugiSaisieDonneesPageComponent implements OnInit {
       'Authorization': `Bearer ${token}` // 🔥 Ajout du token dans l'en-tête
     };
 
-    this.emmissionsFugtivesService.getMachines("test", headers).subscribe({
+    this.emmissionsFugtivesService.getMachines(this.emmissionFugitiveOngletId, headers).subscribe({
       next: (data) => {
         const rawMachines = data.machinesEmissionFugitive;
 
@@ -113,10 +187,6 @@ export class EmissFugiSaisieDonneesPageComponent implements OnInit {
     const token = this.authService.getToken(); // Récupère le token
 
     // Avant d'ajouter la machine, il faut convertir le label en valeur de l'énumération
-    const labelToValueFluide = (label: string) => {
-      return Object.keys(TypeFluide).find(key => TypeFluideLabels[TypeFluide[key as keyof typeof TypeFluide]] === label);
-    };
-
     const labelToValueMachine = (label: string) => {
       return Object.keys(TypeMachineEnum).find(key => TypeMachineLabels[TypeMachineEnum[key as keyof typeof TypeMachineEnum]] === label);
     };
@@ -124,8 +194,6 @@ export class EmissFugiSaisieDonneesPageComponent implements OnInit {
     const machineToAdd = { ...this.newMachine };
 
 // Conversion des labels
-    machineToAdd.typeFluide = labelToValueFluide(machineToAdd.typeFluide);
-
     if (this.newMachine.tauxDeFuiteConnu) {
       machineToAdd.typeMachine = "NA"; // Cas où on ne veut pas de saisie
     } else {
@@ -141,7 +209,7 @@ export class EmissFugiSaisieDonneesPageComponent implements OnInit {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}` // Ajoute le token d'authentification
     };
-
+    console.log(machineToAdd.typeFluide)
     this.emmissionsFugtivesService.addMachine(this.emmissionFugitiveOngletId, machineToAdd, headers).subscribe(() => {
       this.loadMachines();
       this.resetForm();
