d9b85573ecf980693b535039835b830fc2cbb36c
Clovis CHATRY <clovis.chatry@student.junia.com>
2025-06-05 15:37:49 +0200

    Add achat models and mapper service

--- frontend/src/app/components/saisie-donnees-page/achats/achats-saisie-donnees-page.component.ts
+++ frontend/src/app/components/saisie-donnees-page/achats/achats-saisie-donnees-page.component.ts
@@ -7,6 +7,7 @@ import { ApiEndpoints } from '../../../services/api-endpoints';
 import { CommonModule } from '@angular/common';
 import { SaveFooterComponent } from '../../save-footer/save-footer.component';
 import { OngletStatusService } from '../../../services/onglet-status.service';
+import { AchatMapperService } from './achat-mapper.service';
 
 @Component({
   selector: 'app-achats-saisie-donnees-page',
@@ -20,6 +21,7 @@ export class AchatsSaisieDonneesPageComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private authService = inject(AuthService);
   private statusService = inject(OngletStatusService);
+  private mapper = inject(AchatMapperService);
 
   items: any = {};
   estTermine = false;
@@ -48,61 +50,15 @@ export class AchatsSaisieDonneesPageComponent implements OnInit {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
     };
-
-    this.http.get<any>(ApiEndpoints.AchatsOnglet.getById(id), { headers }).subscribe(
-      (data) => {
-        this.items = {
-          // Consommables
-          papierCons: data.papierCons,
-          livresCons: data.livresCons,
-          cartonNeufCons: data.cartonNeufCons,
-          cartonRecycleCons: data.cartonRecycleCons,
-          fournituresCons: data.fournituresCons,
-          jetEncreConsTonnes: null,
-          jetEncreConsRamettes: null,
-          tonerConsTonnes: null,
-          tonerConsRamettes: null,
-          jetEncreCons: data.feuillesJetEncreCons,
-          tonerCons: data.feuillesTonerCons,
-          pharmaCons: data.pharmaCons,
-          servicesCons: data.servicesCons,
-          serviceEnseignementCons: data.serviceEnseignementCons,
-          serviceProduitsInformatiqueCons: data.serviceProduitsInformatiqueCons,
-          serviceReparationsMachinesCons: data.serviceReparationsMachinesCons,
-          serviceTransportCons: data.serviceTransportCons,
-          serviceHebergementRestaurationCons: data.serviceHebergementRestaurationCons,
-          serviceTelecomCons: data.serviceTelecomCons,
-
-          // Textile
-          papierTextile: data.papierTextile,
-          livresTextile: data.livresTextile,
-          cartonNeufTextile: data.cartonNeufTextile,
-          cartonRecycleTextile: data.cartonRecycleTextile,
-          fournituresTextile: data.fournituresTextile,
-          jetEncreTextileTonnes: null,
-          jetEncreTextileRamettes: null,
-          tonerTextileTonnes: null,
-          tonerTextileRamettes: null,
-          jetEncreTextile: data.feuillesJetEncreTextile,
-          tonerTextile: data.feuillesTonerTextile,
-          pharmaTextile: data.pharmaTextile,
-
-          // Restauration
-          hasBilanCarbone: data.bilanCarboneRestauration !== null,
-          bilanCarboneTotal: data.bilanCarboneRestauration,
-          connaitQuantiteAliments: data.connaitQuantiteAliment,
-          boeuf: data.quantiteBoeuf,
-          poisson: data.quantitePoisson,
-          fromage: data.quantiteFromage,
-          boisson: data.quantiteBoisson,
-          connaitRepasParType: data.connaitRepasParType,
-          repasAnimal: data.repasAnimal,
-          repasVegetal: data.repasVegetal,
-          repasVegetarien: data.repasVegetarien
-        };
+    this.http.get<any>(ApiEndpoints.AchatsOnglet.getById(id), { headers }).subscribe({
+      next: data => {
+        this.items = this.mapper.fromDto(data);
+        this.estTermine = this.items.estTermine ?? false;
       },
-      (err) => console.error("Erreur de chargement", err)
-    );
+      error: err => console.error("Erreur de chargement", err)
+    });
+
+
   }
 
   updateAchat(): void {
@@ -115,55 +71,10 @@ export class AchatsSaisieDonneesPageComponent implements OnInit {
       Authorization: `Bearer ${token}`
     };
 
-    const payload = {
-      papierCons: this.items.papierCons,
-      livresCons: this.items.livresCons,
-      cartonNeufCons: this.items.cartonNeufCons,
-      cartonRecycleCons: this.items.cartonRecycleCons,
-      fournituresCons: this.items.fournituresCons,
-      jetEncreConsTonnes: this.items.jetEncreConsTonnes,
-      jetEncreConsRamettes: this.items.jetEncreConsRamettes,
-      tonerConsTonnes: this.items.tonerConsTonnes,
-      tonerConsRamettes: this.items.tonerConsRamettes,
-      feuillesJetEncreCons: this.items.jetEncreCons,
-      feuillesTonerCons: this.items.tonerCons,
-      pharmaCons: this.items.pharmaCons,
-      servicesCons: this.items.servicesCons,
-      serviceEnseignementCons: this.items.serviceEnseignementCons,
-      serviceProduitsInformatiqueCons: this.items.serviceProduitsInformatiqueCons,
-      serviceReparationsMachinesCons: this.items.serviceReparationsMachinesCons,
-      serviceTransportCons: this.items.serviceTransportCons,
-      serviceHebergementRestaurationCons: this.items.serviceHebergementRestaurationCons,
-      serviceTelecomCons: this.items.serviceTelecomCons,
-
-      papierTextile: this.items.papierTextile,
-      livresTextile: this.items.livresTextile,
-      cartonNeufTextile: this.items.cartonNeufTextile,
-      cartonRecycleTextile: this.items.cartonRecycleTextile,
-      fournituresTextile: this.items.fournituresTextile,
-      jetEncreTextileTonnes: this.items.jetEncreTextileTonnes,
-      jetEncreTextileRamettes: this.items.jetEncreTextileRamettes,
-      tonerTextileTonnes: this.items.tonerTextileTonnes,
-      tonerTextileRamettes: this.items.tonerTextileRamettes,
-      feuillesJetEncreTextile: this.items.jetEncreTextile,
-      feuillesTonerTextile: this.items.tonerTextile,
-      pharmaTextile: this.items.pharmaTextile,
-
-      bilanCarboneRestauration: this.items.hasBilanCarbone ? this.items.bilanCarboneTotal : null,
-      connaitQuantiteAliment: this.items.connaitQuantiteAliments,
-      quantiteBoeuf: this.items.boeuf,
-      quantitePoisson: this.items.poisson,
-      quantiteFromage: this.items.fromage,
-      quantiteBoisson: this.items.boisson,
-      connaitRepasParType: this.items.connaitRepasParType,
-      repasAnimal: this.items.repasAnimal,
-      repasVegetal: this.items.repasVegetal,
-      repasVegetarien: this.items.repasVegetarien,
-      estTermine: this.estTermine
-    };
-
+    const payload = this.mapper.toDto({ ...this.items, estTermine: this.estTermine });
     this.http.patch(ApiEndpoints.AchatsOnglet.update(id), payload, { headers }).subscribe({
-      error: err => console.error('PATCH achats échoué', err)
+      error: err => console.error("PATCH achats échoué", err)
     });
+
   }
 }
