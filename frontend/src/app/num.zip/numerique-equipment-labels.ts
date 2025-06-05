f0d327add8345d9f0f0c2002748cb52c09c0330a
Clovis CHATRY <clovis.chatry@student.junia.com>
2025-06-05 17:04:09 +0200

    Align numeric equipment names with backend

--- frontend/src/app/num.zip/numerique-equipment-labels.ts
+++
@@ -1,9 +0,0 @@
-import { NUMERIQUE_EQUIPEMENT } from './enums/numerique.enum';
-
-export const NumeriqueEquipmentLabels: Record<NUMERIQUE_EQUIPEMENT, string> = {
-  [NUMERIQUE_EQUIPEMENT.ECRAN]: 'Écran',
-  [NUMERIQUE_EQUIPEMENT.ORDINATEUR]: 'Ordinateur',
-  [NUMERIQUE_EQUIPEMENT.SERVEUR]: 'Serveur',
-  [NUMERIQUE_EQUIPEMENT.ROUTEUR]: 'Routeur',
-  [NUMERIQUE_EQUIPEMENT.SWITCH]: 'Switch',
-};
