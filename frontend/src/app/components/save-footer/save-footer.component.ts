a0cf729db2d8d27740c03d1a71f91bdd18d48895
Clovis CHATRY <clovis.chatry@student.junia.com>
2025-06-05 11:58:46 +0200

    Add Terminé button and dashboard status sync (#40)

--- frontend/src/app/components/save-footer/save-footer.component.ts
+++ frontend/src/app/components/save-footer/save-footer.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OngletStatusService } from '../../services/onglet-status.service';
 
 @Component({
   selector: 'app-save-footer',
@@ -8,13 +9,30 @@ import { CommonModule } from '@angular/common';
   templateUrl: './save-footer.component.html',
   styleUrls: ['./save-footer.component.scss']
 })
-export class SaveFooterComponent {
+export class SaveFooterComponent implements OnInit {
+  @Input() path = '';
   loading = false;
+  estTermine = false;
+
+  constructor(private statusService: OngletStatusService) {}
+
+  ngOnInit(): void {
+    if (this.path) {
+      this.estTermine = this.statusService.getStatus(this.path);
+    }
+  }
 
   save(): void {
     this.loading = true;
     setTimeout(() => {
       this.loading = false;
     }, 2000);
+  }
+
+  toggleTermine(): void {
+    this.estTermine = !this.estTermine;
+    if (this.path) {
+      this.statusService.setStatus(this.path, this.estTermine);
+    }
   }
 }
