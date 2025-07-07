ca3b22a33372706fcef20601cc1bb4258ef70428
Hugo  Sanson <hugo.sanson@student.junia.com>
2025-07-07 18:25:05 +0200

    feat: endpoints parametres manque refresh

--- frontend/src/app/components/params/params.component.ts
+++ frontend/src/app/components/params/params.component.ts
@@ -1,58 +1,115 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import {Component, inject, ViewChild} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule, NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
+import {UserService} from '../../services/user.service';
+import {ParamService} from './params.service';
+import {AuthService} from '../../services/auth.service';
+import {UtilisateurDto} from '../../models/user.model'; // adapte le chemin selon ton arborescence
 
 interface User {
   firstName: string;
   lastName: string;
   email: string;
   isParams: boolean;
-  isAdmin?: boolean; // bien écrit avec A majuscule
+  isAdmin?: boolean;
 }
 
 interface Entity {
+  id: number;
   name: string;
   type: string;
   params: User;
-  admin: User; // AJOUTÉ pour corriger le HTML
+  admin: User;
+}
+
+export interface EntityNomId {
+  id: number;
+  nom: string;
 }
 
 @Component({
   selector: 'app-params',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule
-  ],
+  imports: [CommonModule, FormsModule],
   templateUrl: './params.component.html',
   styleUrls: ['./params.component.scss']
 })
 export class ParamsComponent {
-  constructor(private router: Router) {}
+  private router = inject(Router);
+  private userService = inject(UserService);
+  private paramService = inject(ParamService);
+  private authService = inject(AuthService);
+
+  isAdmin = this.userService.isAdmin;
+  isSuperAdmin = this.userService.isSuperAdmin;
+  utilisateursEntiteSelectionnee: UtilisateurDto[] = [];
+  @ViewChild('form') form!: NgForm;
 
   user: User = {
-    firstName: '',
-    lastName: '',
     email: '',
+    lastName: '',
+    firstName: '',
     isParams: false
   };
 
-  entity: Entity = {
+  entityToCreate = {
     name: '',
     type: '',
-    params: { firstName: '', lastName: '', email: '', isParams: true },
-    admin: { firstName: '', lastName: '', email: '', isParams: false, isAdmin: false }
+    admin: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      isParams: false,
+      isAdmin: true
+    }
+  };
+
+  userToAdd: { estAdmin: boolean; entityName: string; prenom: string; nom: string; email: string } = {
+    entityName: '',
+    prenom: '',
+    nom: '',
+    email: '',
+    estAdmin: false
   };
 
   newUserEntity: string = '';
   selectedEntity: string = '';
 
   entities: Entity[] = [];
+  entitiesList: EntityNomId[] = [];
   users: { entity: string; user: User }[] = [];
 
+  ngOnInit(): void {
+    this.loadUtilisateursParEntite(this.userService.entiteId());
+    this.loadEntitiesIfSuperAdmin();
+  }
+
+
+  getAuthHeaders(): { [key: string]: string } {
+    const token = this.authService.getToken();
+    return {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    };
+  }
+
+  isEmailValid(email: string): boolean {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  }
+
   updateInfo(): void {
-    console.log('Infos mises à jour :', this.user);
+    const userId = this.userService.rawUser().id;
+    const headers = this.getAuthHeaders();
+    this.paramService.updateUserInfos(userId, {
+      prenom: this.user.firstName,
+      nom: this.user.lastName,
+      email: this.user.email
+    }, headers).subscribe({
+      next: () => alert("Mise à jour effectuée."),
+      error: (err) => console.error('Erreur lors de la mise à jour :', err)
+    });
   }
 
   sendPasswordReset(): void {
@@ -60,30 +117,107 @@ export class ParamsComponent {
   }
 
   createEntity(): void {
-    this.entities.push({ ...this.entity });
-    console.log('Nouvelle entité :', this.entity);
-
-    this.entity = {
-      name: '',
-      type: '',
-      params: { firstName: '', lastName: '', email: '', isParams: true },
-      admin: { firstName: '', lastName: '', email: '', isParams: false, isAdmin: false }
+    const body = {
+      nom: this.entityToCreate.name,
+      type: this.entityToCreate.type,
+      nomUtilisateur: this.entityToCreate.admin.lastName,
+      prenomUtilisateur: this.entityToCreate.admin.firstName,
+      emailUtilisateur: this.entityToCreate.admin.email
     };
+
+    const headers = this.getAuthHeaders();
+
+    this.paramService.creerEntite(body, headers).subscribe({
+      next: (response) => {
+        alert('Entité créée avec succès !');
+        this.entities.push({
+          id: response.id,
+          name: this.entityToCreate.name,
+          type: this.entityToCreate.type,
+          params: {firstName: '', lastName: '', email: '', isParams: true},
+          admin: {...this.entityToCreate.admin}
+        });
+        this.form.resetForm();
+      },
+      error: (err) => console.error('Erreur HTTP :', err)
+    });
   }
 
   addUser(): void {
-    if (this.newUserEntity) {
-      this.users.push({ entity: this.newUserEntity, user: { ...this.entity.admin } });
-      console.log('Utilisateur ajouté à', this.newUserEntity, ':', this.entity.admin);
+    const headers = this.getAuthHeaders();
+    const entiteId = this.userService.entiteId();
+    const body = {
+      prenom: this.userToAdd.prenom,
+      nom: this.userToAdd.nom,
+      email: this.userToAdd.email,
+      estAdmin: this.userToAdd.estAdmin
+    };
 
-      this.entity.admin = { firstName: '', lastName: '', email: '', isParams: false, isAdmin: false };
+    this.paramService.creerutilisateur(entiteId, body, headers)
+      .subscribe({
+        next: () => {
+          alert('Utilisateur créé avec succès');
+          this.form.resetForm();
+          this.loadEntitiesIfSuperAdmin();
+        },
+        error: (err) => {
+          console.error('Erreur lors de la création', err);
+          this.form.resetForm();
+        }
+      });
+  }
+
+  loadUtilisateursParEntite(entiteId: number): void {
+    const headers = this.getAuthHeaders();
+
+    this.paramService.getUtilisateurParEntiteId(entiteId, headers).subscribe({
+      next: (users: UtilisateurDto[]) => {
+        this.utilisateursEntiteSelectionnee = users;
+        console.log('Utilisateurs chargés :', users);
+      },
+      error: (err) => {
+        console.error('Erreur lors du chargement des utilisateurs :', err);
+      }
+    });
+  }
+
+  loadEntitiesIfSuperAdmin(): void {
+    const headers = this.getAuthHeaders();
+    this.paramService.getAllEntiteNomId(headers).subscribe({
+      next: (entites: { id: number, nom: string }[]) => {
+        this.entitiesList = entites;
+        console.log('Entités chargées :', this.entitiesList);
+      },
+      error: (err) => {
+        console.error('Erreur lors du chargement des entités :', err);
+      }
+    });
+
+  }
+
+  onEntityChange(entiteId: number): void {
+    if (entiteId) {
+      this.loadUtilisateursParEntite(entiteId);
     }
   }
 
-  filteredUsers(): User[] {
-    return this.users
-      .filter(u => u.entity === this.selectedEntity)
-      .map(u => u.user);
+  // Permet de gérer le check du rôle Admin dans la liste des utilisateurs
+  onToggleAdmin(utilisateur: UtilisateurDto): void {
+    const headers = this.getAuthHeaders();
+    const entiteId = this.userService.entiteId();
+
+    // Nouvelle valeur à envoyer
+    const nouvelleValeur = !utilisateur.estAdmin;
+
+    this.paramService.modifierEstAdmin(utilisateur.id, nouvelleValeur, headers)
+      .subscribe({
+        next: () => {
+          utilisateur.estAdmin = nouvelleValeur; // mise à jour locale après succès
+        },
+        error: (err) => {
+          alert('Erreur lors de la mise à jour du rôle administrateur.');
+        }
+      });
   }
 
   goBackToDashboard(): void {
