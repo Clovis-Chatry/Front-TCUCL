8a54dd7c94706d96df1768411ea075b33075caae
Clovis CHATRY <clovis.chatry@student.junia.com>
2025-07-09 10:35:01 +0200

    Display synthese EGES global values

--- frontend/src/app/services/synthese-eges.service.ts
+++ frontend/src/app/services/synthese-eges.service.ts
@@ -5,7 +5,18 @@ import { ApiEndpoints } from './api-endpoints';
 import { AuthService } from './auth.service';
 
 export interface SyntheseEgesResult {
-  consoEnergieFinale: number;
+  emissionFugitivesGlobal: number | null;
+  energieGlobal: number | null;
+  mobiliteDomicileTravailGlobal: number | null;
+  autreMobiliteFrGlobal: number | null;
+  mobiliteInternationalGlobal: number | null;
+  batimentParkingGlobal: number | null;
+  numeriqueGlobal: number | null;
+  autreImmobilisationGlobal: number | null;
+  achatGlobal: number | null;
+  dechetGlobal: number | null;
+  bilanCarboneTotalGlobal: number | null;
+  consoEnergieFinale?: number | null;
 }
 
 @Injectable({ providedIn: 'root' })
