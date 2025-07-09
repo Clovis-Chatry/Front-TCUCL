b9d80b1bbbd1ab69fccbfb704e6599dd8536e812
clovis.chatry <clovis.chatry@student.junia.com>
2025-07-09 10:58:41 +0200

    mob internationale

--- frontend/src/app/services/api-endpoints.ts
+++ frontend/src/app/services/api-endpoints.ts
@@ -67,8 +67,8 @@ ParkingVoirieOnglet: {
   getResult: (id: string) => `${BASE_URL}/${ONGLET_KEYS.Parkings}/${id}/resultat`
 },
 mobInternationaleOnglet: {
-  getById: (id: string) => `${BASE_URL}/mobInternationaleOnglet/${id}`,
-  update: (id: string) => `${BASE_URL}/mobInternationaleOnglet/${id}`
+  getById: (id: string) => `${BASE_URL}/${ONGLET_KEYS.MobInternationale}/${id}`,
+  update: (id: string) => `${BASE_URL}/${ONGLET_KEYS.MobInternationale}/${id}`
 },
 BatimentsOnglet: {
   getBatimentImmobilisationMobilier: (id: string) =>
