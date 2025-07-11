e37d4ba45b84604283663a455127bc12a13089c0
clovis.chatry <clovis.chatry@student.junia.com>
2025-07-11 16:39:33 +0200

    Final app

--- frontend/src/app/services/api-endpoints.ts
+++ frontend/src/app/services/api-endpoints.ts
@@ -1,6 +1,6 @@
 import {ONGLET_KEYS} from '../constants/onglet-keys';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://trajectoirecarbone.univ-catholille.fr';
 
 export const ApiEndpoints = {
   Onglets: {
